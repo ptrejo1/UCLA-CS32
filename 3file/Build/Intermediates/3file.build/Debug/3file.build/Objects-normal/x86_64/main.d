949392d967504e85d4bb3c407b84d43e9f16095f
Phoenix Trejo <phoenixtrejo@yahoo.com>
2018-03-31 15:48:45 -0700

    h4

--- 3file/Build/Intermediates/3file.build/Debug/3file.build/Objects-normal/x86_64/main.d
+++
@@ -1,2 +0,0 @@
-dependencies: /Users/phoenixtrejo/Desktop/fun/3file/3file/main.cpp \
-  /Users/phoenixtrejo/Desktop/fun/3file/3file/stu.h
--- 3file/Build/Intermediates/3file.build/Debug/3file.build/Objects-normal/x86_64/student.d
+++
@@ -1,2 +0,0 @@
-dependencies: /Users/phoenixtrejo/Desktop/fun/3file/3file/student.cpp \
-  /Users/phoenixtrejo/Desktop/fun/3file/3file/stu.h
